97d3f1294f8bd6131444c774dde9ea5870539d77
imshivamb <shivambhardwaj1503@gmail.com>
2024-05-30 14:40:14 +0530

    new section added

--- app/components/store-section/StoreSectionThree.tsx
+++ app/components/store-section/StoreSectionThree.tsx
@@ -1,59 +1,108 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import { Button } from "../Button";
 import { WandSparkles } from "lucide-react";
-import { features2, featuresimage } from "../../constants/imagesImport";
+import {
+  features1,
+  features2,
+  featuresimage,
+} from "../../constants/imagesImport";
 
 const StoreSectionThree = () => {
+  const [currentImage, setCurrentImage] = useState(features1);
+  const [clickedDiv, setClickedDiv] = useState(null);
+
+  const handleImageChange = (image, divIndex) => {
+    setCurrentImage(image);
+    setClickedDiv(divIndex);
+  };
+
+  const getClassNames = (divIndex) => {
+    return `first-click-div hover:text-gray-950  hover:opacity-100 cursor-pointer flex flex-col gap-3 w-full pb-5 border-b border-gray-300 ${
+      clickedDiv === divIndex ? "opacity-100" : "opacity-80"
+    }`;
+  };
   return (
-    <div className="mx-auto w-full px-4 md:px-12 main-container mt-6 md:mt-16">
+    <div className="mx-auto w-full px-4 md:px-6 main-container mt-6 md:mt-16">
       <div className="flex flex-col md:flex-row items-start justify-start gap-5 w-full px-0 md:px-6 py-4">
         <div className="block w-full p-3 md:p-5 md:w-1/2 relative min-h-[500px]">
           <Image
-            src={features2}
+            src={currentImage}
             alt="store feature"
             className="object-cover rounded-xl w-full"
           />
         </div>
         <div className="flex w-full md:w-1/2 flex-col p-2 md:p-5 gap-4 items-start justify-start">
           <div className="mb-0 bg-gray-200 text-gray-800 font-medium text-xs rounded-full  px-3 py-1">
-            Notifications
+            Selling Products
           </div>
           <h3 className="text-4xl md:text-5xl/tight font-medium max-w-[20ch]">
-            Real time notifications on account activity
+            Sell your products or source new items to sell
           </h3>
-          <p className="text-base/normal font-medium text-gray-800 max-w-[40ch]">
-            Centralize and simplify payments, and get comprehensive insights on
-            your financials. Connect your bank account and your card to
-            Milestone.
-          </p>
-          <div className="flex gap-4 w-full items-stretch pt-6 flex-col md:flex-row">
-            <div className="w-full flex flex-col shadow-md rounded-xl bg-white px-5 py-4">
-              <div className="flex items-center gap-3">
-                <p className="text-2xl font-semibold">₹999</p>
-                <div className="bg-green-100/80 flex gap-1.5 items-center text-green-500 font-medium text-base rounded-full  px-3 py-2">
-                  <WandSparkles size={20} /> <span>Up to 25%</span>
-                </div>
-              </div>
-              <p className="text-base max-w-[25ch] pt-5 font-medium">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <div className="flex flex-col gap-4 mt-4 w-full">
+            <div
+              className={getClassNames(1)}
+              onClick={() => handleImageChange(features2, 1)}
+            >
+              <h4 className="text-2xl font-semibold text-gray-800 leading-normal">
+                Sell Physical Products
+              </h4>
+              <p className="font-medium text-base leading-[1.5]">
+                Market and sell physical products directly from your fully
+                responsive e-commerce website. Customizable themes, integrate
+                payment gateways, upload products in bulk, manage inventory,
+                process orders, and reach customers worldwide with ease.
               </p>
             </div>
-            <div className="w-full flex flex-col shadow-md rounded-xl bg-white px-5 py-4">
-              <div className="flex items-center gap-3">
-                <p className="text-2xl font-semibold">₹2,999</p>
-                <div className="bg-green-100/80 flex gap-1.5 items-center text-green-500 font-medium text-base rounded-full  px-3 py-2">
-                  <WandSparkles size={20} /> <span>Up to 25%</span>
-                </div>
-              </div>
-              <p className="text-base max-w-[25ch] pt-5 font-medium">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <div
+              className={getClassNames(2)}
+              onClick={() => handleImageChange(featuresimage, 2)}
+            >
+              <h4 className="text-2xl font-semibold text-gray-800 leading-normal">
+                Dropshipping
+              </h4>
+              <p className="font-medium text-base leading-[1.5]">
+                Start your dropshipping business with minimal investment and
+                zero inventory management. Connect with suppliers, list products
+                from third party apps, and focus on marketing and sales while
+                suppliers will handle the logistics.
+              </p>
+            </div>
+            <div
+              className={getClassNames(3)}
+              onClick={() => handleImageChange(features1, 3)}
+            >
+              <h4 className="text-2xl font-semibold text-gray-800 leading-normal">
+                Print on Demand
+              </h4>
+              <p className="font-medium text-base leading-[1.5]">
+                Launch your print-on-demand business with ease. Design,
+                customize, and sell unique products without holding inventory.
+                From t-shirts to headphones etc, Let suppliers handle the
+                manufacturing and shipping, while you focus on creating and
+                marketing your custom merchandise.
+              </p>
+            </div>
+            <div
+              className={getClassNames(4)}
+              onClick={() => handleImageChange(featuresimage, 4)}
+            >
+              <h4 className="text-2xl font-semibold text-gray-800 leading-normal">
+                Sell Digital Products
+              </h4>
+              <p className="font-medium text-base leading-[1.5]">
+                Monetize your digital creations effortlessly. From music files
+                to ebooks, offer a diverse range of digital products to your
+                audience.
               </p>
             </div>
           </div>
-          <div className="my-5">
+
+          {/* <div className="my-5">
             <Button text="Get Started" className="text-white bg-gray-800" />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- app/components/themes section/ThemesSection.tsx
+++ app/components/themes section/ThemesSection.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import { Button } from "../Button";
 import {
   theme1,
@@ -14,8 +16,28 @@ import {
 } from "../../constants/imagesImport";
 
 const ThemesSection = () => {
+  const scrollContainer = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollContainer.current) {
+      scrollContainer.current.scrollBy({
+        left: -300, // Adjust this value as needed
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainer.current) {
+      scrollContainer.current.scrollBy({
+        left: 300, // Adjust this value as needed
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div className="w-full pt-12 pb-8 mt-5 md:mt-6 bg-[#CDE4E1] ">
+    <div className="w-full pt-12 pb-8 mt-5 md:mt-6 bg-[#CDE4E1]">
       <div className="main-container p-2 md:p-8 w-full">
         <div className="flex flex-col md:flex-row justify-between items-center w-full px-4 py-3">
           <div className="float-left w-full max-w-xl">
@@ -36,69 +58,99 @@ const ThemesSection = () => {
           </div>
         </div>
       </div>
-      <div
-        className="pl-6 md:pl-16  w-full overflow-hidden overflow-x-auto flex  h-full pr-6"
-        // style={{ scrollbarWidth: "none" }}
-      >
-        <div className="relative flex justify-start items-start w-full pb-10 gap-8 pr-6">
-          <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[325px] md:max-w-[500px]">
-            <Image
-              src="/images/13.png"
-              alt="theme 1"
-              width={500}
-              height={325}
-              className="w-full hover:scale-105 object-cover object-center transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
-            />
-          </div>
-          <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[100px] md:max-w-[170px]">
-            <Image
-              src="/images/16.png"
-              alt="theme 1"
-              width={170}
-              height={325}
-              className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
-            />
-          </div>
-          <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[325px] md:max-w-[500px]">
-            <Image
-              src="/images/14.png"
-              alt="theme 1"
-              width={500}
-              height={325}
-              className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
-            />
-          </div>
-          <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[325px] md:max-w-[500px]">
-            <Image
-              src="/images/15.png"
-              alt="theme 1"
-              width={500}
-              height={325}
-              className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
-            />
-          </div>
-          <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[100px] md:max-w-[170px]">
-            <Image
-              src="/images/theme2.jpg"
-              alt="theme 1"
-              width={170}
-              height={325}
-              className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
-            />
-          </div>
-          <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[100px] md:max-w-[170px]">
-            <Image
-              src="/images/theme2.jpg"
-              alt="theme 1"
-              width={170}
-              height={325}
-              className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
-            />
+      <div className="relative">
+        <div
+          ref={scrollContainer}
+          className="pl-6 md:pl-16  w-full overflow-hidden overflow-x-auto flex  h-full pr-6"
+          style={{ scrollbarWidth: "none" }}
+        >
+          <div className="relative flex justify-start items-start w-full pb-10 gap-8 pr-6">
+            <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[325px] md:max-w-[500px]">
+              <Image
+                src="/images/13.png"
+                alt="theme 1"
+                width={500}
+                height={325}
+                className="w-full hover:scale-105 object-cover object-center transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
+              />
+            </div>
+            <div className="relative max-h-[200px] md:max-h-[325px] md:h-[325px] h-full w-full flex-shrink-0 max-w-[100px] md:max-w-[170px]">
+              <Image
+                src="/images/16.png"
+                alt="theme 1"
+                width={170}
+                height={325}
+                className="w-full hover:scale-105 transition-all md:h-[325px] duration-500 h-full flex flex-shrink-0 rounded-xl"
+              />
+            </div>
+            <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[325px] md:max-w-[500px]">
+              <Image
+                src="/images/14.png"
+                alt="theme 1"
+                width={500}
+                height={325}
+                className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
+              />
+            </div>
+            <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[325px] md:max-w-[500px]">
+              <Image
+                src="/images/15.png"
+                alt="theme 1"
+                width={500}
+                height={325}
+                className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
+              />
+            </div>
+            <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[100px] md:max-w-[170px]">
+              <Image
+                src="/images/17.png"
+                alt="theme 1"
+                width={170}
+                height={325}
+                className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
+              />
+            </div>
+            <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[100px] md:max-w-[170px]">
+              <Image
+                src="/images/18.png"
+                alt="theme 1"
+                width={170}
+                height={325}
+                className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
+              />
+            </div>
+            <div className="relative max-h-[200px] md:max-h-[325px] h-full w-full flex-shrink-0 max-w-[325px] md:max-w-[500px]">
+              <Image
+                src="/images/19.png"
+                alt="theme 1"
+                width={500}
+                height={325}
+                className="w-full hover:scale-105 transition-all duration-500 h-full flex flex-shrink-0 rounded-xl"
+              />
+            </div>
           </div>
         </div>
       </div>
-      <div className=" pl-6 md:pl-16 mt-6">
+      <div className="flex items-center justify-between pl-6 md:pl-16 mt-6">
         <Button text="Explore all Themes" className="bg-gray-900 text-white" />
+        <div className="flex items-center gap-3 pr-8">
+          <div className=" flex items-center pl-2">
+            <button
+              onClick={scrollLeft}
+              className="bg-gray-900 text-white rounded-full p-2 px-5 hover:bg-gray-700 transition"
+            >
+              &lt;
+            </button>
+          </div>
+          <div className=" flex items-center pr-2">
+            <button
+              onClick={scrollRight}
+              className="bg-gray-900 text-white rounded-full p-2  px-5 hover:bg-gray-700 transition"
+            >
+              &gt;
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
